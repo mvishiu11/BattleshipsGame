cf72251dd6a4ba59f1bd7f1dceeff9a54ef77498
mvishiu11 <116217532+mvishiu11@users.noreply.github.com>
2024-03-15 14:23:02 +0100

    fix: resolve neigh finding issue

--- lab1_1/board.cpp
+++ lab1_1/board.cpp
@@ -352,10 +352,6 @@ void board::set_neighbours(ship* s)
 				{
 					b_fields[i][j + 1].type = field_state::neutral;
 				}
-				if (j < grid_size - 1 and i < grid_size - 1 and b_fields[i + 1][j + 1].type == field_state::empty)
-				{
-					b_fields[i][j + 1].type = field_state::neutral;
-				}
 				if (j > 0 and i > 0 and b_fields[i - 1][j - 1].type == field_state::empty)
 				{
 					b_fields[i - 1][j - 1].type = field_state::neutral;
